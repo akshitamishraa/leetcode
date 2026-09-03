4e83f58f1916181b1d96a6b56d5db3a293043794
Akshita Mishra <akshita9477@gmail.com>
2026-09-03 22:19:54 +0530

    Time: 3 ms (42.36%), Space: 43.5 MB (8.5%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char [] s1 = s.toCharArray();
+        char [] t1 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        int i = 0;
+        int j = 0;
+        while(i < s1.length && j < t1.length){
+            if(s1[i] == t1[i]){
+                i++;
+                j++;
+            } else {
+                return t1[j];
+            }
+        }
+        return t1[j];
+    }
+}
